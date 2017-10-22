dcfa17959695b7eef545adaa2cd168848b8433d2
Nathan Minissale <Bitterpilot@users.noreply.github.com>
2017-10-22 13:20:35 +0800

    Lesson 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int[] myArray = {1,2,3,4};
+        String[] myStringArray = {"hello","hi","what"};
+
+        //Arrays have length
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println("Item #" + myArray[i]);
+        }
     }
 }
